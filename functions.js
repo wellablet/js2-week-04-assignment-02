7b8fe6474366d207c2170938c65211fd04cc3805
Ablet Adem <ablet0991@hotmail.com>
2022-12-08 13:53:18 +0300

    assignment done

--- functions.js
+++ functions.js
@@ -1,3 +1,16 @@
-const isTheWordSplit = () => {};
-
-export { isTheWordSplit };
+const isTheWordSplit = (pArr) => {
+    let condition = "not possible"
+    const baseWord = pArr[0];
+    const otherWords = pArr[1].split(",");
+    for (let index = 0; index < otherWords.length; index++) {
+      for (let index2 = 0; index2 < otherWords.length; index2++) {
+          if (otherWords[index]+otherWords[index2]===baseWord){
+              condition = otherWords[index].concat(",",otherWords[index2])
+          }
+      }
+    }
+    return condition
+  };
+  export {
+      isTheWordSplit
+  };
